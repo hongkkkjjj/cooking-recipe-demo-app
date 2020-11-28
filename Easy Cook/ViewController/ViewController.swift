d389e002f48d32fe8d268373effe9a79e24d42d1
Kua Jun Hong <jhkua@financial-link.com>
2020-11-28 14:47:08 +0800

    Pod init

---
+++ Easy Cook/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Easy Cook
+//
+//  Created by Kua Jun Hong on 27/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Easy Cook/Pods-Easy Cook-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Easy_Cook : NSObject
+@end
+@implementation PodsDummy_Pods_Easy_Cook
+@end
